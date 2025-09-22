512eac6e9d5a65b78c9fd03e1658d950384bce89
Pablo Felipe <pablofsc@outlook.com>
2025-09-22 00:24:47 -0300

    separa métodos em SimulacaoService

--- src/main/java/org/pablofsc/service/SimulacaoService.java
+++ src/main/java/org/pablofsc/service/SimulacaoService.java
@@ -20,28 +20,46 @@ public class SimulacaoService {
       throw new IllegalArgumentException("Produto não encontrado");
     }
 
-    // Validar prazo
     if (simulacao.prazoMeses > produto.prazoMaximoMeses) {
       throw new IllegalArgumentException("Prazo solicitado (" + simulacao.prazoMeses + " meses) excede o limite do produto (" + produto.prazoMaximoMeses + " meses)");
     }
 
-    // Calcular taxa efetiva mensal
-    double taxaAnual = produto.taxaJurosAnual / 100.0;
-    double taxaEfetivaMensal = Math.pow(1 + taxaAnual, 1.0 / 12.0) - 1;
+    double taxaEfetivaMensal = calcularTaxaEfetivaMensal(produto);
+    double parcela = calcularParcela(simulacao, taxaEfetivaMensal);
+    double valorTotal = calcularValorTotal(parcela, simulacao.prazoMeses);
+    List<MemoriaCalculo> memoria = calcularMemoriaCalculo(simulacao, taxaEfetivaMensal, parcela);
 
-    // Calcular parcela mensal PRICE
+    return new SimulacaoResponse(
+      produto,
+      (double) simulacao.valorSolicitado,
+      simulacao.prazoMeses,
+      Math.round(taxaEfetivaMensal * 100000.0) / 100000.0,
+      Math.round(valorTotal * 100.0) / 100.0,
+      Math.round(parcela * 100.0) / 100.0,
+      memoria
+    );
+  }
+
+  private double calcularTaxaEfetivaMensal(ProdutoEntity produto) {
+    double taxaAnual = produto.taxaJurosAnual / 100.0;
+    return Math.pow(1 + taxaAnual, 1.0 / 12.0) - 1;
+  }
+
+  private double calcularParcela(SimulacaoRequest simulacao, double taxaEfetivaMensal) {
     double valor = simulacao.valorSolicitado;
     int prazo = simulacao.prazoMeses;
-    double parcela = valor * (taxaEfetivaMensal * Math.pow(1 + taxaEfetivaMensal, prazo)) / (Math.pow(1 + taxaEfetivaMensal, prazo) - 1);
+    return valor * (taxaEfetivaMensal * Math.pow(1 + taxaEfetivaMensal, prazo)) / (Math.pow(1 + taxaEfetivaMensal, prazo) - 1);
+  }
 
-    // Valor total com juros
-    double valorTotal = parcela * prazo;
+  private double calcularValorTotal(double parcela, int prazo) {
+    return parcela * prazo;
+  }
 
-    // Calcular memória de cálculo
+  private List<MemoriaCalculo> calcularMemoriaCalculo(SimulacaoRequest simulacao, double taxaEfetivaMensal, double parcela) {
     List<MemoriaCalculo> memoria = new ArrayList<>();
-    double saldo = valor;
+    double saldo = simulacao.valorSolicitado;
 
-    for (int mes = 1; mes <= prazo; mes++) {
+    for (int mes = 1; mes <= simulacao.prazoMeses; mes++) {
       double juros = saldo * taxaEfetivaMensal;
       double amortizacao = parcela - juros;
       double saldoFinal = saldo - amortizacao;
@@ -59,14 +77,6 @@ public class SimulacaoService {
       saldo = saldoFinal;
     }
 
-    return new SimulacaoResponse(
-      produto,
-      (double) simulacao.valorSolicitado,
-      simulacao.prazoMeses,
-      Math.round(taxaEfetivaMensal * 100000.0) / 100000.0,
-      Math.round(valorTotal * 100.0) / 100.0,
-      Math.round(parcela * 100.0) / 100.0,
-      memoria
-    );
+    return memoria;
   }
 }
